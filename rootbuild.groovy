8232afc7688e8e839a2626620d92dd3ca042f015
hmorcos <hanymorcos@live.com>
2018-09-05 17:33:15 -0400

    remove maven

--- rootbuild.groovy
+++ rootbuild.groovy
@@ -8,7 +8,7 @@ node{
            cleanWs()
            checkout scm    // still needed, otherwise the following sh "git ..." commands will not work
 
-           sh "ls *.json > listJsonFiles"
+           sh "ls *.* > listJsonFiles"
    def files = readFile( "listJsonFiles" ).split( "\\r?\\n" );
    sh "rm -f listJsonFiles"
           print "printing" . files
